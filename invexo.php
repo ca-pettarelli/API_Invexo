1d631b7d71d6ae1c93a8792382f189d76a654d9c
caroline_dev <https://caroline_dev@bitbucket.org>
2020-11-25 23:12:27 -0300

    last step

--- invexo.php
+++ invexo.php
@@ -1,43 +1,15 @@
 <?php
     include_once 'negocios.php';
 
-    // $message = passthru("./scripts/teste.py -pag 2 -regs 2 2>&1");
-    // print_r($message);
-    // echo gettype($message);
-
-    // $arr = json_decode($message);
-    // var_dump($arr);
-    // echo $arr; 
-
-
-    // function console_log($output, $with_script_tags = true) {
-    //     $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . ');';
-    //     if ($with_script_tags) {
-    //         $js_code = '<script>' . $js_code . '</script>';
-    //     }
-    //     echo $js_code;
-    // }
     //verifica a página atual caso seja informada na URL, senão atribui como 1ª página 
     $pagina = (isset($_GET['pagina']))? $_GET['pagina'] : 1; 
     $pagina = intval($pagina);
     
-    // echo "Tipo pagina no invexo.php topo: ".gettype($pagina)."\n";
-    // echo 'PAGINA : '.$pagina;
     $registros = 10;
     $result = Negocios::index($pagina, $registros);
-    // print_r($result);
-    // echo count($result);
-    // print_r($result);
-    
-    // foreach ($result as $i => $value) {
-        // print_r($i);
-        // print_r($result[$i]);
-    // }
-    // print_r($result);    
 
     if (count($result) != 10) {
         $total = $pagina;
-        echo 'ACABOU';
     } else{
         $total = $pagina + 1;
     }
@@ -50,94 +22,6 @@
     $inicio = ((($pagina - $lim) > 1)? $pagina - $lim : 1); 
     $fim = ((($pagina+$lim) < $numPaginas) ? $pagina+$lim : $numPaginas);
 
-    // $url = "https://api.moskitcrm.com/v2/deals/search";
-    // $data = array("field"=>"CF_lXODObivipvANmaN","expression"=>"all_of", "values"=> array("165206"));
-    // $data = "{\"field\" : \"CF_lXODObivipvANmaN\", \"expression\" : \"all_of\", \"values\" : [\"165206\"] }";
-    // $options = array(
-    //     'http' => array(
-    //         'header'  => array( 'apikey:  168ec8df-5e4f-440f-b3cd-d03b1039dff7', 'Content-Type: application/json'),
-    //         'method'  => 'POST',
-    //         'content' => http_build_query($data)
-    //     )
-    // );
-    // $context  = stream_context_create($options);
-    // $teste = file_get_contents($url, false, $context);
-
-    // $postdata = json_encode($data);
-
-    // $curl = curl_init($url);
-    // curl_setopt($curl, CURLOPT_POST, true);
-    // curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
-    // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    // curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-    //     'apikey: 168ec8df-5e4f-440f-b3cd-d03b1039dff7',
-    //     'Content-Type: application/json',
-    //     ));
-    // $response = curl_exec($curl);
-    // curl_close($curl);
-    // $teste = $response;
-
-    // print_r($teste);
-    // console_log($teste);
-    // echo var_dump(http_response_code());
-
-    // $url = 'url_to_post';
-    // $data = array("first_name" => "First name","last_name" => "last name","email"=>"email@gmail.com","addresses" => array ("address1" => "some address" ,"city" => "city","country" => "CA", "first_name" =>  "Mother","last_name" =>  "Lastnameson","phone" => "555-1212", "province" => "ON", "zip" => "123 ABC" ) );
-
-    // $postdata = json_encode($data);
-
-    // $ch = curl_init($url);
-    // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    // curl_setopt($ch, CURLOPT_POST, 1);
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, array('apikey: 168ec8df-5e4f-440f-b3cd-d03b1039dff7', 'Content-Type: application/json'));
-    // $teste = curl_exec($ch);
-    // curl_close($ch);
-    // print_r($teste);
-
-
-
-    // $data = array("first_name" => "First name","last_name" => "last name","email"=>"email@gmail.com","addresses" => array ("address1" => "some address" ,"city" => "city","country" => "CA", "first_name" =>  "Mother","last_name" =>  "Lastnameson","phone" => "555-1212", "province" => "ON", "zip" => "123 ABC" ) );
-
-    // $data_string = json_encode(array("customer" =>$data));
-
-    // $ch = curl_init($url);
-
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'apikey: 168ec8df-5e4f-440f-b3cd-d03b1039dff7'));
-
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    // $teste = curl_exec($ch);
-
-    // curl_close($ch);
-
-    // echo "$teste";
-
-
-    // $data = array("field"=>"CF_lXODObivipvANmaN","expression"=>"all_of", "values"=> array("165206"));                                                                    
-    // $data_string = json_encode($data);                                                                                   
-                                                                                                                        
-    // $ch = curl_init('https://api.moskitcrm.com/v2/deals/search');                                                                      
-    // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    //     'apikey: 168ec8df-5e4f-440f-b3cd-d03b1039dff7',                                                                          
-    //     'Content-Type: application/json',                         
-    // ));                                                                                                                   
-                                                                                                                        
-    // $teste = curl_exec($ch);
-    // curl_close($ch);
-
-    // $final = json_decode($teste);
-
-    // print_r($final);
-    
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -197,8 +81,6 @@
 
                 <tbody>
                     <?php foreach ($result as $user):
-                        // print_r($user);
-                        // print_r($user->name);
                         $date = explode('T', $user->dateCreated);
                         $dia = $date[0];
                         $hora = $date[1];
@@ -212,11 +94,8 @@
                         $H = intval(explode(':', $horario)[0]) - 3;
                         $M = explode(':', $horario)[1];
                         $S = explode(':', $horario)[2];
-
                         $finalHora = $H . ':'. $M . ':' . $S;
 
-                        // print_r($user->dateCreated);
-                        // $finalDate = $date->format('d-m-Y H:i:s') . "\n";
                         $finalDate = $finaldia .' '. $finalHora ."\n";
                         $Negocio = '';
                         $Empreendimento = '';
@@ -226,7 +105,6 @@
                             $Empreendimento = $name[1];
                             $Negocio = $name[0];
                         }
-                        // print_r($Empreendimento);
                     ?>
                     <tr>
                         <?php include_once 'customFields.php';
@@ -237,12 +115,8 @@
                         $Mensagem = '';
                         
                         foreach ($user->entityCustomFields as $values): 
-                            // print_r($values);
-
                             if ($values->id == 'CF_42AmaJiZCW64LDjl'){
                                 $Mensagem = $values->textValue;
-                                // echo $Mensagem;
-
                             }
                             if ($values->id == 'CF_ylAm0viKi37pqvbx'){
                                 if ($Empreendimento == ''){
@@ -255,9 +129,7 @@
                             if ($values->id  == 'CF_Pj3qYeidiNxLqQeb'){
                                 $endpoint = '/customFields/CF_Pj3qYeidiNxLqQeb/options/'. $values->options[0]; 
                                 $value = CustomFields::index($endpoint);
-                                // print_r($value->label);
                                 $Bairro = $value->label;
-                                // echo $Bairro;
                             }
                             if ($values->id == 'CF_GwyMgWi9CWAzzMLA'){
                                 $Origem = $values->textValue;
@@ -269,9 +141,6 @@
                                     $Qualificação = 'Ruim';
                                 }
                             }
-                            // if ($values->customField->name == 'Mensagem'){
-                            //     $Mensagem = $values->value;
-                            // }
                         ?>
                         <?php endforeach ?>
                         <td scope="row"><?= $Negocio ?></td>
--- negocios.php
+++ negocios.php
@@ -7,23 +7,6 @@ class Negocios
         $limit = $registros;
         $start = ($pagina - 1) * $limit;
 
-        // $end = 112;
-        // for ($i = 1; $i <= 7; $i++) {
-        // $cURLConnection = curl_init('https://api.moskitcrm.com/v1/deals/?start=' . $start . '&limit=' . $limit . '&order=desc');
-        // curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
-        // curl_setopt($cURLConnection, CURLOPT_HTTPHEADER, array(
-        // 'apikey: a4ca266a-ef70-462f-a5bd-abc6340928b0',
-        // ));
-
-        // $apiResponse = curl_exec($cURLConnection);
-        // curl_close($cURLConnection);
-
-        // $jsonObj = json_decode($apiResponse);
-
-        // // $end = $jsonObj->metadata->pagination->total;
-        // // $start = $start + 100;
-        // $result = $jsonObj;
-        // API URL
         $url = 'https://api.moskitcrm.com/v2/deals/search/?start' . $start . '&limit=' . $limit . '&order=desc';
 
         // Create a new cURL resource
@@ -38,8 +21,7 @@ class Negocios
         // $payload = json_encode(array("user" => $data));
         $payload = json_encode($data);
         $payload = '['. $payload . ']';
-        // echo gettype($payload);
-        // echo $payload;
+
         // Attach encoded JSON string to the POST fields
         curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 
@@ -48,48 +30,14 @@ class Negocios
 
         // Return response instead of outputting
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-        // curl_setopt($ch, CURLOPT_HEADER, true);
 
         // Execute the POST request
         $result = curl_exec($ch);
         
-        // $teste = curl_getinfo($ch, CURLINFO_HEADER_OUT);
-        // var_dump($teste);
-        // $teste = curl_multi_getcontent($ch);
-
-        // $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-        // $headers = substr($result, 0, $header_size);
-        // $body = substr($result, $header_size);
-
-        
-        // print_r(apache_request_headers());
-
-        // $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-        // $header = substr($result, 0, $headerSize);
-        // $header = getHeaders($header);
-
-        // $headers = explode("\r\n", $headers); // The seperator used in the Response Header is CRLF (Aka. \r\n) 
-
-        // $headers = array_filter($headers);
-
-        // $html = '';
-        // foreach ($headers as &$value) {
-        //     $html .= '<li>' . $value . '</li>';
-        // }
-        // $html = '<ol>' . $html . '</ol>';
-
-        // header("Content-Type:text/html; charset=UTF-8");
-        // echo $html;
         // Close cURL resource
         curl_close($ch);
 
         $resultado = json_decode($result);
-
-
-        // print_r($result);
-        // echo gettype($result);
-        // echo gettype($resultado);
 
         return $resultado; 
     }
